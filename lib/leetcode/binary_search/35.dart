006578d9ffd94187f01fd4fb4bf9f487c4505161
Piyal <syedanamul67@gmail.com>
2025-03-10 15:55:36 +0600

    35-Search Insert Position

---
+++ lib/leetcode/binary_search/35.dart
@@ -0,0 +1,30 @@
+///
+/// Search Insert Position
+/// binary search
+///
+
+void main() {
+  List<int> nums = [1, 3, 5, 6];
+  int target = 5;
+
+  int result = seachPosition(nums, target);
+  print(result);
+}
+
+int seachPosition(List<int> list, int target) {
+  int start = 0;
+  int end = list.length - 1;
+
+  while (start <= end) {
+    int mid = end + (start - end) ~/ 2;
+
+    if (list[mid] == target) {
+      return mid;
+    } else if (list[mid] < target) {
+      start = mid + 1;
+    } else {
+      end = mid - 1;
+    }
+  }
+  return -1;
+}
