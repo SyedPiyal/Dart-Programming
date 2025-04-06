cd81ca2b42d4b974562c2ce952c0a637e27e8f94
Piyal <syedanamul67@gmail.com>
2025-04-06 16:20:19 +0600

    variable name change

--- lib/leetcode/binary_search/34.dart
+++ lib/leetcode/binary_search/34.dart
@@ -4,7 +4,7 @@
 ///
 
 void main() {
-  List<int> nums = [5, 7, 7, 8, 8, 10];
+  List<int> nums = [5, 6,7, 8, 9, 10];
   int target = 8;
   var result = searchPosition(nums, target);
   print(result);
@@ -16,12 +16,12 @@ List<int> searchPosition(List<int> nums, int target) {
   return left == right ? [-1, -1] : [left, right - 1];
 }
 
-int search(List<int> nums, int x) {
+int search(List<int> nums, int target) {
   int left = 0;
   int right = nums.length - 1;
   while (left <= right) {
     int mid = left + (right - left) ~/ 2;
-    if (nums[mid] >= x) {
+    if (nums[mid] >= target) {
       right = mid - 1;
     } else {
       left = mid + 1;
